95618e1c5711451d66f1236e9ca50db72396c71d
Avinash Bissoondial <68248623+bissoondial@users.noreply.github.com>
2024-09-14 07:47:24 -0400

    Updated Title

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ file_transcribe = gr.Interface(
                     gr.Checkbox(label="Use API", value=False)  # Checkbox for API usage
                 ],
                 outputs=["text",gr.Textbox(label="Time taken", type="text")],  # Placeholder for transcribed text and time taken
-                title="Whisper Large V3: Transcribe Audio",
+                title="Welcome to QuickTranscribe.",
                 description=(
                     "Transcribe long-form microphone or audio inputs with the click of a button!"
                 ),
