5eec0c856935f8f0be72f0612c0df2ecb13c4313
VenkateshRoshan <42509229+VenkateshRoshan@users.noreply.github.com>
2024-10-07 18:54:35 -0400

    systemProcess Updated

--- setup_systemProcess.sh
+++ setup_systemProcess.sh
@@ -8,7 +8,7 @@ USER=student-admin
 ssh_key_path=./
 old_ssh_key_name=./student-admin_key
 
-# remove keys if already present
+# # remove keys if already present
 rm -f $ssh_key_path/my_key*
 
 # TODO : Add password to the ssh key and store it in a file and read the file and get the password
@@ -33,23 +33,21 @@ echo "$new_ssh_pub_key"
 # Use the old SSH key to connect to the server and append the new public key to authorized_keys
 ssh -i "$old_ssh_key_name" -p "$PORT" "$USER@$HOST" "echo '$new_ssh_pub_key' >> /home/student-admin/.ssh/authorized_keys && chmod 600 /home/student-admin/.ssh/authorized_keys"
 
-# TODO : Comment the old ssh key in the authorized_keys file
-# 
-
 # Verify if the key has been added successfully
 echo "New public key added to authorized_keys on the server."
 
+new_ssh_key_name=./my_key
+
 # Make a variable to store the SSH connection command with 
-SSH_CONNECTION="ssh -i my_key -p $PORT $USER@$HOST"
+SSH_CONNECTION="ssh -i $new_ssh_key_name -p $PORT $USER@$HOST"
 
 # run a command to create apt install python3-venv
-# ssh -i "$old_ssh_key_name" -p "$PORT" "$USER@$HOST" "sudo apt-get update && sudo apt-get install python3-venv"
-$SSH_CONNECTION "sudo apt-get update && suod apt-get upgrade && sudo apt-get install python3-venv && pip install --upgrade pip"
+$SSH_CONNECTION "sudo apt-get update && sudo apt-get upgrade && sudo apt-get install python3-venv && pip install --upgrade pip"
 
 VENV=/home/$USER/mlops
 
-# creating a virtual environment
-# ssh -i "$old_ssh_key_name" -p "$PORT" "$USER@$HOST" "python3 -m venv $VENV"
+# # creating a virtual environment
+# # ssh -i "$old_ssh_key_name" -p "$PORT" "$USER@$HOST" "python3 -m venv $VENV"
 $SSH_CONNECTION "python3 -m venv $VENV"
 
 echo "Virtual environment created. in $VENV"
@@ -61,25 +59,31 @@ GIT_REPO_PATH=https://github.com/VenkateshRoshan/MLOPs-CaseStudy1.git
 # ssh -i "$old_ssh_key_name" -p "$PORT" "$USER@$HOST" "git clone $GIT_REPO_PATH"
 $SSH_CONNECTION "git clone $GIT_REPO_PATH"
 
+$SSH_CONNECTION "export HF_TOKEN=$HF_TOKEN; echo \$HF_TOKEN >> ~/.bashrc; source ~/.bashrc"
+
 # Activate the virtual environment
 # ssh -i "$old_ssh_key_name" -p "$PORT" "$USER@$HOST" "source $VENV/bin/activate && sudo apt install ffmpeg && cd MLOPs-CaseStudy1 && pip install -r requirements.txt"
-$SSH_CONNECTION "source $VENV/bin/activate && sudo apt install ffmpeg && cd MLOPs-CaseStudy1 && pip3 install -r requirements.txt" # python3 app.py" to run the application
+$SSH_CONNECTION "source $VENV/bin/activate && sudo apt install ffmpeg && cd MLOPs-CaseStudy1 && pip3 install --no-cache-dir -r requirements.txt" # python3 app.py" to run the application
 
 echo "The environment is ready to run the application."
 
 # check the pip list in the environment
 $SSH_CONNECTION "source $VENV/bin/activate && pip list"
+$SSH_CONNECTION "export HF_TOKEN=$HF_TOKEN; echo \$HF_TOKEN >> ~/.bashrc; source ~/.bashrc"
+# add $HF_TOKEN to /home/$USER/.env
+$SSH_CONNECTION "echo 'HF_TOKEN=$HF_TOKEN' >> /home/$USER/.env"
 
 # Create a systemd service file for the application
 SERVICE_FILE="[Unit]
-Description= <<< MLOps Case Study 1 Deployment >>>
+Description=MLOps Case Study 1 Deployment
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/bin/bash -c 'source $VENV/bin/activate && cd /home/$USER/MLOPs-CaseStudy1 && python3 app.py'
+ExecStart=/bin/bash -c 'source ~/.bashrc && source $VENV/bin/activate && cd /home/student-admin/MLOPs-CaseStudy1 && python app.py'
 Restart=always
 User=$USER
+EnvironmentFile=/home/$USER/.env
 WorkingDirectory=/home/$USER/MLOPs-CaseStudy1
 
 [Install]
@@ -87,6 +91,9 @@ WantedBy=multi-user.target"
 
 # Write the service file to the server
 echo "$SERVICE_FILE" | $SSH_CONNECTION "sudo tee /etc/systemd/system/mlopscs2.service > /dev/null"
+
+# give permission to the service file
+$SSH_CONNECTION "sudo chmod 644 /etc/systemd/system/mlopscs2.service"
 
 # Reload systemd to recognize the new service
 $SSH_CONNECTION "sudo systemctl daemon-reload"
@@ -104,7 +111,7 @@ $SSH_CONNECTION "sudo systemctl start mlopscs2"
 # $SSH_CONNECTION "sudo systemctl restart mlopscs2"
 
 # Run the application
-$SSH_CONNECTION "source $VENV/bin/activate && cd MLOPs-CaseStudy1 && python3 app.py"
+# $SSH_CONNECTION "source $VENV/bin/activate && cd MLOPs-CaseStudy1 && python3 app.py"
 
 echo "Systemd service for MLOps application created and started."
 
